082beb38cb978e81e6d2476785b7f68708b077db
johndahrah <dahrah@yandex.ru>
2018-12-26 17:08:30 +0300

    Initial Commit

---
+++ Equalizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Equalizer
+//
+//  Created by Иван Дахненко on 26.12.2018.
+//  Copyright © 2018 Ivan Dakhnenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
